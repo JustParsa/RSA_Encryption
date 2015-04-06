166fe218e6ba94352ea714501579ee1ea02d9f3a
Parsa Alipour <parsaalipour@hotmail.com>
2015-04-06 17:24:32 -0400

    got rid of random bracket

--- src/write_to_file.py
+++ src/write_to_file.py
@@ -8,6 +8,6 @@ def write_to_file(raw_message, encoded_message, ciphertext, n, e, d):
     f.write(str(ciphertext) + '\n\n')
     f.write('PUBLIC KEY:\n')
     f.write('({0} , {1})\n\n'.format(n,e))
-    f.write('PRIVATE KEY:)\n')
+    f.write('PRIVATE KEY:\n')
     f.write('({0} , {1})\n\n'.format(n,d))
     f.write('==================================================\n\n')
